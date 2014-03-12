7dc9c2f9f85afeb092caab4ce6254e3de283f24d
Philipp Koehn <pkoehn@inf.ed.ac.uk>
2014-03-12 14:15:23 -0400

    download cat and mt server

--- install/download-test-model.sh
+++ install/download-test-model.sh
@@ -1,44 +1,15 @@
 #!/bin/sh
 
-echo 'downloading and installing gui backend'
-if [ -d /opt/casmacat/web-server ]
-then
-  cd /opt/casmacat/web-server
-  git pull
-  git checkout casmacat
-else
-  cd /opt/casmacat
-  git clone git://git.assembla.com/matecat_source.git web-server
-  cd web-server
-  git checkout casmacat
-  mkdir downloads
-  chmod o+w downloads
-  mkdir uploads
-  chmod o+w uploads
-  mkdir logs
-  chmod o+w logs
-fi
+mkdir -p /opt/casmacat/engines
 
-# Install CAT Server
-echo 'downloading and installing cat server'
-if [ -d /opt/casmacat/cat-server ]
+echo 'downloading test model'
+if [ -d /opt/casmacat/test ]
 then
-  cd /opt/casmacat/cat-server
-  git pull
+  echo 'already downloaded'
+  # delete and re-download?
 else
-  cd /opt/casmacat
-  git clone git://github.com/hsamand/casmacat-cat-server.git cat-server
-fi
-cd /opt/casmacat/cat-server
-g++ main.cpp -o predict
-
-# Install MT Server
-echo 'downloading and installing mt server'
-if [ -d /opt/casmacat/mt-server ]
-then
-  cd /opt/casmacat/mt-server
-  git pull
-else
-  cd /opt/casmacat
-  git clone git://github.com/christianbuck/matecat_util.git mt-server
+  cd /opt/casmacat/engines
+  wget http://www.casmacat.eu/uploads/toy-fr-en.tgz
+  tar xzf toy-fr-en.tgz
+  mkdir /opt/casmacat/engines/log
 fi
--- install/install-casmacat.sh
+++ install/install-casmacat.sh
@@ -21,6 +21,7 @@ fi
 
 # Install CAT Server
 echo 'downloading and installing cat server'
+apt-get -yq install python-tornado
 if [ -d /opt/casmacat/cat-server ]
 then
   cd /opt/casmacat/cat-server
@@ -29,11 +30,25 @@ else
   cd /opt/casmacat
   git clone git://github.com/hsamand/casmacat-cat-server.git cat-server
 fi
+
 cd /opt/casmacat/cat-server
 g++ main.cpp -o predict
 
+if [ -d /opt/casmacat/cat-server/tornadio2 ]
+then
+  cd /opt/casmacat/cat-server/tornadio2
+  git pull
+else
+  cd /opt/casmacat/cat-server
+  git clone git://github.com/mrjoes/tornadio2
+fi
+cd /opt/casmacat/cat-server/tornadio2
+python setup.py install
+
 # Install MT Server
 echo 'downloading and installing mt server'
+apt-get -yq install python-pip
+pip install CherryPy
 if [ -d /opt/casmacat/mt-server ]
 then
   cd /opt/casmacat/mt-server
@@ -42,3 +57,4 @@ else
   cd /opt/casmacat
   git clone git://github.com/christianbuck/matecat_util.git mt-server
 fi
+
