f32ae51bc07086184d4dcf66f8ce0a9f7fa165f3
Vicent Alabau <valabau@gmail.com>
2014-06-30 10:47:26 +0200

    Disable Moses installation for debigging purposes

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+INSTALL_MOSES=no
+INSTALL_THOT=yes
+
 cd /opt/casmacat/install
 mkdir -p log/install
 
@@ -8,6 +11,14 @@ chown -R www-data:www-data /opt/casmacat
 sh ./install-admin.sh > log/install/admin.out 2> log/install/admin.err &
 
 sh ./install-dependencies2.sh >> log/install/dependencies.out 2>> log/install/dependencies.err
-sh ./install-moses.sh > log/install/moses.out 2> log/install/moses.err &
-sh ./install-casmacat.sh > log/install/casmacat.out 2> log/install/casmacat.err &
-sh ./download-test-model.sh > log/install/test-model.out 2> log/install/test-model.err &
+
+if test "$INSTALL_MOSES" = "yes"; then 
+  sh ./install-moses.sh > log/install/moses.out 2> log/install/moses.err &
+  sh ./install-casmacat.sh > log/install/casmacat.out 2> log/install/casmacat.err &
+  sh ./download-test-model.sh > log/install/test-model.out 2> log/install/test-model.err &
+fi
+
+if test "$INSTALL_THOT" = "yes"; then 
+  sh ./install-casmacat-upvlc.sh > log/install/casmacat-upvlc.out 2> log/install/casmacat-upvlc.err 
+  sh ./install-thot.sh > log/install/thot.out 2> log/install/thot.err 
+fi
