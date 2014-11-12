9b6d637010d718f22d576d645e5275b83b2a5f42
Vicent Alabau <valabau@prhlt.upv.es>
2014-11-12 18:11:26 +0100

    Change to phikoehn github

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 apt-get -yq update
 apt-get -yq install git
-git clone https://github.com/valabau/casmacat-install.git /opt/casmacat
+git clone https://github.com/phikoehn/casmacat-install.git /opt/casmacat
 cd /opt/casmacat/install
 sh install.sh
